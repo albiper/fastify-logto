a92eebab241961ef49af523de831c1cb2bdd32e7
Alberto Perona <albi.per91@gmail.com>
2025-05-24 00:43:39 +0200

    working build
    Signed-off-by: Alberto Perona <albi.per91@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,25 @@
-export { default } from '../../rollup.config.js';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { summary } from 'rollup-plugin-summary';
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const configs = {
+    input: ['src/index.ts'],
+    output: [
+        {
+            format: 'esm',
+            dir: 'lib',
+            preserveModules: true,
+            exports: 'named',
+            entryFileNames: '[name].js',
+            interop: 'auto',
+        },
+        { dir: 'lib', preserveModules: true },
+    ],
+    plugins: [typescript({ tsconfig: 'tsconfig.build.json' }), nodeResolve(), summary()],
+    external: [/node_modules/, /@logto/],
+};
+
+export default configs;
